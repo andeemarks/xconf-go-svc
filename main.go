c44d0cb496838f9a28f2d13f507620d36c110c92
Andy Marks <amarks@thoughtworks.com>
2014-07-31 20:53:58 +1000

    Allowing port configuration through env. var.

--- main.go
+++ main.go
@@ -4,18 +4,24 @@ import (
 	"log"
 	"log/syslog"
 	"net/http"
-
+	"os"
 	"github.com/emicklei/go-restful"
 	"github.com/emicklei/go-restful/swagger"
 )
+
 
 func main() {
 	u := UserService{map[string]User{}}
 	u.Register()
 
+	port := os.Getenv("PORT")
+	if (port == "") {
+		port = "8080"
+	}
+
 	config := swagger.Config{
 		WebServices:    restful.RegisteredWebServices(), // you control what services are visible
-		WebServicesUrl: "http://localhost:8080",
+		WebServicesUrl: "http://localhost:" + port,
 		ApiPath:        "/apidocs.json"}
 
 	swagger.InstallSwaggerService(config)
@@ -25,6 +31,6 @@ func main() {
 		log.SetOutput(logwriter)
 	}
 
-	log.Printf("start listening on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("start listening on localhost:" + port)
+	log.Fatal(http.ListenAndServe(":" + port, nil))
 }
